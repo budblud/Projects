48a91a2896c4be2ff98296b9ea878228c131b7db
budblud <illiagp2xl@gmail.com>
2021-10-28 20:50:51 +0200

    nothin
    nothin

--- python_trading/binance_future.py
+++ python_trading/binance_future.py
@@ -1,3 +1,22 @@
 import logging
+import requests
+import pprint
 
 logger = logging.Logger
+
+def get_contracts():
+    response_object = requests.get('https://testnet.binancefuture.com/dapi/v1/exchangeInfo')
+
+    print(response_object.status_code)
+
+    contracts = []
+    for contract in response_object.json()['symbols']:
+        contracts.append(contract['pair'])
+
+    return contracts
+
+
+
+print(get_contracts())
+
+
--- python_trading/main.py
+++ python_trading/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk # helps us to create the window
 import logging # is used for receiving messages about errors, bugs etc.
-
+from binance_future import *
 logger = logging.getLogger()
 
 #stream_handler = logging.StreamHandler()
@@ -12,19 +12,20 @@ formatter = logging.Formatter(# формат сообщений, которые 
     "%(asctime)s %(levelname)s :: %(message)s"
     )
 
-file_handler = logging.FileHandler('info.log')
-file_handler.setFormatter(formatter)
-file_handler.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('info.log') #создает файл в котором выводятся сообщения(file handler = файл оброботчик# )
+file_handler.setFormatter(formatter) #внутри файла ставит формат
+file_handler.setLevel(logging.DEBUG) # ставит уровень читаемости сообщений(какие сообщения читать)
 
 #logger.addHandler(stream_handler)
-logger.addHandler(file_handler)
-logger.setLevel(logging.INFO)
-logger.info("This message shows when that code was executed.")
+logger.addHandler(file_handler) #добавляет в Логгер наш файл
+logger.setLevel(logging.INFO) # в логгере ставит уровень читаемоси сообщений
+logger.info("This message shows when that code was executed.") # само сообщение
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("My_First_Window_made_for_Binance")
     root.mainloop() # mainloop prevent closing the window
 
+    print(get_contracts())
 # Изучить!
 
